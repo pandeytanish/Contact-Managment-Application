81c7e238ea29cf29d0e0b3541a5e744d8332ac65
Tanish Pandey <tanishpandey14@gmail.com>
2024-10-23 21:27:01 +0530

    final commit

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -6,72 +6,72 @@ const User = require("../models/userModel");
 //@route Post /api/users/register
 //@access public
 
-const registerUsers = asyncHandler(async (req,res) => {
-    const {username,email,password} = req.body;
-    if(!username || !email || !password) {
+const registerUsers = asyncHandler(async (req, res) => {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
         res.status(400);
         throw new Error("All fields are required")
     }
-    const userAvailable = await User.findOne({email});
-    if(userAvailable) {
+    const userAvailable = await User.findOne({ email });
+    if (userAvailable) {
         res.status(400);
         throw new Error("User already exsists")
     }
-  //Hash Password 
-  const hashedPassword = await bcrypt.hash(password,10);
-  console.log("Hashed Password : ",hashedPassword)
-  const user = await User.create({
-    username,
-    email,
-    password : hashedPassword
-  });
+    //Hash Password 
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log("Hashed Password : ", hashedPassword)
+    const user = await User.create({
+        username,
+        email,
+        password: hashedPassword
+    });
 
-  console.log(`user created ${user}`)
-  if(user) {
-    res.status(201).json({_id : user.id, email: user.email})
-  } else {
-    res.status(400);
-    throw new Error("User data not vALID")
-  }
-    res.json({message : "Register the User"})
+    console.log(`user created ${user}`)
+    if (user) {
+        res.status(201).json({ _id: user.id, email: user.email })
+    } else {
+        res.status(400);
+        throw new Error("User data not vALID")
+    }
+    res.json({ message: "Register the User" })
 });
 
 //@desc login User
 //@route Post /api/users/login
 //@access public
 
-const loginUsers = asyncHandler(async (req,res) => {
-    const {email,password} = req.body;
-    if(!email || !password){
+const loginUsers = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
         res.status(400);
         throw new Error("All fields are required")
     }
-    const user = await User.findOne({email});
+    const user = await User.findOne({ email });
     //compare password with hashed password
-    if(user && (await bcrypt.compare(password, user.password))) {
+    if (user && (await bcrypt.compare(password, user.password))) {
         const accessToken = jwt.sign({
-            user : {
-                username : user.username,
-                email : user.email,
-                id : user.id
+            user: {
+                username: user.username,
+                email: user.email,
+                id: user.id
             },
         }, process.env.ACCESS_TOKEN_SECERT,
-        {expiresIn : "10m"}
-    )
-        res.status(200).json({accessToken});
+            { expiresIn: "10m" }
+        )
+        res.status(200).json({ accessToken });
     } else {
         res.status(401)
         throw new Error("Email or password not valid")
     }
-    res.json({message : "Login User"})
+    res.json({ message: "Login User" })
 });
 
 //@desc Current User
 //@route Post /api/users/login
 //@access private
 
-const currentUsers = asyncHandler(async (req,res) => {
+const currentUsers = asyncHandler(async (req, res) => {
     res.json(req.user)
 });
 
-module.exports = {registerUsers,loginUsers,currentUsers}
+module.exports = { registerUsers, loginUsers, currentUsers }
